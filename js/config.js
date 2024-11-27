72542f4deb176d882b3689826a5ff20f4c521f45
pragya2396 <pragya222@gmail.com>
2024-11-26 18:17:53 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_lAe1ITTVC', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7cphn0q1sv526nqn3naaeare8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_XdChRoJxW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2ju3jokehrqdl1lt9ciaakq904', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
